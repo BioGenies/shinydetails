587c88ef1990a36ba4fb3a39655183f51d14bf8a
KrystynaGrzesiak <krygrz11@gmail.com>
2020-09-17 11:43:52 +0200

    changes creating helpfiles

--- R/ui_functions.R
+++ R/ui_functions.R
@@ -3,17 +3,14 @@
 #' @description  Renders plot with helper within an application.
 #' Same as \code{helper(plotOutput(outputId, ...))}
 #' @inheritParams shiny::plotOutput
-#' @param helpfiles A character string denoting directory to save empty help files.
-#' If \code{NULL} help files will not be created. Default \code{NULL}.
+#' @param helpfiles A character string denoting directory to save empty help files. Default \code{'helpfiles'}.
 #' @param ... Optional arguments for \code{plotOutput}.
 #' @details This function uses \code{\link[shinyhelper]{helper}}.
 #' @export
 #'
 
-plotOutput_h <- function(outputId, helpfiles = NULL, ...) {
-  if(!is.null(helpfiles) & !file.exists(paste0(helpfiles, "/", outputId, ".md"))) {
-    create_help_files(outputId, help_dir = helpfiles)
-  }
+plotOutput_h <- function(outputId, helpfiles = "helpfiles", ...) {
+  create_help_files(outputId, help_dir = helpfiles)
   helper(plotOutput(outputId, ...), content = outputId, type = "markdown")
 }
 
@@ -21,12 +18,13 @@ plotOutput_h <- function(outputId, helpfiles = NULL, ...) {
 #' @description  Constructs a slider widget with helper.
 #' Same as \code{helper(sliderInput(outputId, ...))}
 #' @inheritParams shiny::sliderInput
+#' @inheritParams plotOutput_h
 #' @param ... Optional arguments for \code{sliderInput}.
 #' @details This function uses \code{\link[shinyhelper]{helper}}.
 #' @export
 #'
 
-sliderInput_h <- function(inputId, ...) {
+sliderInput_h <- function(inputId, helpfiles = "helpfiles", ...) {
   helper(sliderInput(inputId, ...), content = inputId, type = "markdown")
 }
 
@@ -34,12 +32,13 @@ sliderInput_h <- function(inputId, ...) {
 #' @description  Constructs a numeric input with helper.
 #' Same as \code{helper(numericInput(outputId, ...))}
 #' @inheritParams shiny::numericInput
+#' @inheritParams plotOutput_h
 #' @param ... Optional arguments for \code{numericInput}.
 #' @details This function uses \code{\link[shinyhelper]{helper}}.
 #' @export
 #'
 
-numericInput_h <- function(inputId, ...) {
+numericInput_h <- function(inputId, helpfiles = "helpfiles", ...) {
   helper(numericInput(inputId, ...), content = inputId, type = "markdown")
 }
 
@@ -47,17 +46,13 @@ numericInput_h <- function(inputId, ...) {
 #' @description  Renders table with helper within an application.
 #' Same as \code{helper(dataTableOutput(outputId, ...))}
 #' @inheritParams shiny::dataTableOutput
-#' @param helpfiles A character string denoting directory to save empty help files.
-#' If \code{NULL} help files will not be created. Default \code{NULL}.
-#' @param ... Optional arguments for \code{dataTableOutput}.
+#' @inheritParams plotOutput_h
 #' @details This function uses \code{\link[shinyhelper]{helper}}.
 #' @export
 #'
 
 
-tableOutput_h <- function(outputId, helpfiles = NULL, ...) {
-  if(!is.null(helpfiles) & !file.exists(paste0(helpfiles, "/", outputId, ".md"))) {
-    create_help_files(outputId, help_dir = helpfiles)
-  }
+tableOutput_h <- function(outputId, helpfiles = "helpfiles", ...) {
+  create_help_files(outputId, help_dir = helpfiles)
   helper(dataTableOutput(outputId, ...), content = outputId, type = "markdown")
 }
