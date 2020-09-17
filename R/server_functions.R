f36164d277bcd79ad63e0155f7780537c2739d11
KrystynaGrzesiak <krygrz11@gmail.com>
2020-09-17 12:38:50 +0200

    changes = to <- :)

--- R/server_functions.R
+++ R/server_functions.R
@@ -33,7 +33,7 @@ dt_format <- function(dat, cols = colnames(dat)) {
 #' @export
 #' @importFrom ggplot2 ggsave
 
-generate_downloadButton = function(id, plot_out, device) {
+generate_downloadButton <- function(id, plot_out, device) {
   downloadHandler(filename = paste0(id, "_plot.", device),
                   content = function(file){
                     ggsave(file, plot_out(), device = device, height = 300,
@@ -53,23 +53,23 @@ generate_downloadButton = function(id, plot_out, device) {
 #' @export
 
 
-prepare_tt_data = function(hv, plot_out, plot_type = "geom_point") {
+prepare_tt_data <- function(hv, plot_out, plot_type = "geom_point") {
 
-  plot_data = plot_out()[["data"]]
+  plot_data <- plot_out()[["data"]]
 
   switch(plot_type,
          geom_col = {
-           plot_data_info = ggplot_build(plot_out())[["data"]][[1]]
-           hv_data = data.frame(x = hv[["x"]],
+           plot_data_info <- ggplot_build(plot_out())[["data"]][[1]]
+           hv_data <- data.frame(x = hv[["x"]],
                                 y = hv[["y"]],
                                 xmin = plot_data_info[["xmin"]],
                                 xmax = plot_data_info[["xmax"]],
                                 plot_data)
-           tt_df = hv_data[hv_data[["x"]] < hv_data[["xmax"]] & hv_data[["x"]] >= hv_data[["xmin"]],
+           tt_df <- hv_data[hv_data[["x"]] < hv_data[["xmax"]] & hv_data[["x"]] >= hv_data[["xmin"]],
                            !(colnames(hv_data) %in% c("x", "y", "xmax", "xmin"))]
          },
          geom_point = {
-           tt_df = nearPoints(df = plot_data, coordinfo = hv, maxpoints = 1)
+           tt_df <- nearPoints(df = plot_data, coordinfo = hv, maxpoints = 1)
          },
          comparison = {
            hv_data <- data.frame(x = hv[["x"]],
@@ -79,7 +79,7 @@ prepare_tt_data = function(hv, plot_out, plot_type = "geom_point") {
                                  y_plot = plot_data[[hv[["mapping"]][["y"]]]],
                                  Sequence = plot_data[["Sequence"]],
                                  State = plot_data[["State"]])
-           tt_df = hv_data[hv_data[["x"]] > hv_data[["Start"]] &
+           tt_df <- hv_data[hv_data[["x"]] > hv_data[["Start"]] &
                              hv_data[["x"]] < hv_data[["End"]] &
                              abs(hv_data[["y_plot"]] - hv_data[["y"]]) < 10 &
                              abs(hv_data[["y_plot"]] - hv_data[["y"]]) == min(abs(hv_data[["y_plot"]] - hv_data[["y"]])),
@@ -111,9 +111,9 @@ prepare_tt_data = function(hv, plot_out, plot_type = "geom_point") {
 #' @export
 #'
 
-generate_tooltip = function(hv, plot_out, plot_type, tt_content) {
+generate_tooltip <- function(hv, plot_out, plot_type, tt_content) {
 
-  tt_df = prepare_tt_data(hv, plot_out, plot_type)
+  tt_df <- prepare_tt_data(hv, plot_out, plot_type)
 
   if(nrow(tt_df) != 0) {
     tt_pos_adj <- ifelse(hv[["coords_img"]][["x"]]/hv[["range"]][["right"]] < 0.5,
@@ -128,12 +128,12 @@ generate_tooltip = function(hv, plot_out, plot_type, tt_content) {
                     "px; top:", hv[["coords_css"]][["y"]], "px; padding: 0px;")
 
     if(is.null(tt_content)) {
-      content = paste(colnames(tt_df), ": ", t(tt_df ), c(rep("<br/>", ncol(tt_df)-1), ""))
+      content <- paste(colnames(tt_df), ": ", t(tt_df ), c(rep("<br/>", ncol(tt_df)-1), ""))
     }else {
-      text = paste(tt_content[["row_text"]],
+      text <- paste(tt_content[["row_text"]],
                    c(rep(" <br/> ", length(tt_content[["row_text"]]) - 1), ""),
                    sep = "", collapse = "")
-      content = do.call(sprintf, c(list(text), lapply(tt_content[["chosen_cols"]], function(i) tt_df[[i]])))
+      content <- do.call(sprintf, c(list(text), lapply(tt_content[["chosen_cols"]], function(i) tt_df[[i]])))
     }
 
     div(style = style,
